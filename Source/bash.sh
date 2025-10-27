dacd985762e2bf1c27fb32991f770204fe116395
HOSEINLOL <184541064+hoseinlolready@users.noreply.github.com>
2025-10-27 16:04:58 +0330

    Add 'Developer Mode active' message to installer

--- Source/bash.sh
+++ Source/bash.sh
@@ -82,6 +82,7 @@ show_menu() {
   echo "============================"
   echo "   Pingtunnel Installer"
   echo "   By HOSEINLOL V 1.0.0"
+  echo "   Developer Mode active "
   echo "============================"
   echo "1) Install / Update Pingtunnel"
   echo "2) Uninstall Pingtunnel"
